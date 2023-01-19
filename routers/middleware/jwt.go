3780c2b0fb0268362c9f8b96e7ac5f2c0d3719e2
Leonardo Sola <lsolaproducoes@gmail.com>
2023-01-19 18:24:30 -0300

    removendo logs

--- routers/middleware/jwt.go
+++ routers/middleware/jwt.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 	"tsbank/auth"
 	"tsbank/models"
@@ -13,9 +12,7 @@ import (
 // Authenticate é responsável por autenticar o usuário
 func Authenticate() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		fmt.Println("Authenticating user", ctx.Request.Header.Get("Authorization"))
 		if erro := auth.ValidateToken(ctx); erro != nil {
-			fmt.Println("Erro ao validar token", erro)
 			responses.Error(ctx, http.StatusUnauthorized, "Token não é valido")
 			ctx.Abort()
 			return
